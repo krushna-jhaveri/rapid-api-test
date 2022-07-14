c71d9315ef8d1c5570d397e2ba411fc2b5ab980a
Krushna Jhaveri <kdp200789@gmail.com>
2022-07-15 01:34:39 +0200

    [WIP] Updates the test for app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders  Example 1 link', () => {
+    render(<App />)
+    const linkElement = screen.getByText('Example 1')
+    expect(linkElement).toBeInTheDocument()
+})
+
+test('renders  Example 2 link', () => {
+    render(<App />)
+    const linkElement = screen.getByText('Example 2')
+    expect(linkElement).toBeInTheDocument()
+})
